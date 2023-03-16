01f3e3ce156fe136a472b6b73326ef64495b570c
kuzantiv <kuzantiv1992@mail.ru>
2023-03-16 22:42:32 +0600

    добавил еще один класс исключений

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,10 @@
 class APIRequestError(Exception):
     """Вызывается при ошибке запроса к API Practicum."""
-    pass
+
+    ...
+
+
+class HomeworkStatusError(Exception):
+    """Вызывается при ошибке статуса ответа API Practicum."""
+
+    ...
